9c9ecb37d5d61caf4c37ed1880cb26c0d9fe5a6b
Kameron Weaver <finerthanafroghair@gmail.com>
2012-01-22 21:44:20 -0700

    finished level 5 of ruby warrior

--- project02/rubywarrior/kameron-weaver-beginner/player.rb
+++ project02/rubywarrior/kameron-weaver-beginner/player.rb
@@ -13,7 +13,11 @@ class Player
 			warrior.walk!
 		end
 	else
-		warrior.attack!
+		if(warrior.feel.captive?)
+                        warrior.rescue!
+                else
+			warrior.attack!
+		end
 	end
 	@lastHealth = warrior.health
   end
