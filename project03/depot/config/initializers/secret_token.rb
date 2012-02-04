eab59466bc43e4a6fb354db9726d0a123d279449
Kameron Weaver <kaweaver@mines.edu>
2012-02-04 16:50:20 -0700

    got rvm, rails, and ruby1.9.2 installed. created new depot and generated scaffold

---
+++ project03/depot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '13b89e8c87ea8fe16ce2678fbcce8b76a60e24d34351ddd73a19eb3fb85548203e3570c19e07aeb6c14c460d851f65b62ca4abb71f96cd52c498c41c79c0fdf8'
