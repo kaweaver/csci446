9aeb247918d081b61e9e47c153dce3841e42446c
Kameron Weaver <kaweaver@mines.edu>
2012-02-12 20:31:00 -0700

    Adding the initial info for the articl.es app

--- project03/depot/app/controllers/carts_controller.rb
+++ project03/depot/app/controllers/carts_controller.rb
@@ -90,7 +90,7 @@ class CartsController < ApplicationController
     session[:cart_id] = nil
     respond_to do |format|
       format.html { redirect_to store_url }
-      format.json { head :ok }
+      format.json { head :no_content }
     end
   end
 end
---
+++ project04/articles/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Articles::Application.config.secret_token = 'e696cf66d4b487e257f5036673037b8e510aec86e2d0e3d7d6336ca7f34dacba2564df21c13ea606c4d5ac3b5f0ed5c7fe75245b93a129a8469b00c64c9d8d2b'
