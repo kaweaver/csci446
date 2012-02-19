26ecfa88d5a83ea42217844d48457a22e334fb1e
Kameron Weaver <kaweaver@mines.edu>
2012-02-19 12:27:59 -0700

    created the basic outline and added 1 article, as well as making the header the same

---
+++ project04/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Articles::Application.config.secret_token = '1a8f4742e9cb40466d2a4bf2c6ea0769da57e9e9c3e9e3a8e9a27adb69ad5ebae3a81670f95bc4767d2c3112991f7ea77a15bd3369454d45ac27905561c9b85d'
