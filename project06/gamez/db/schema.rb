57b25eade9069147c0e9fd4aa6c3259b888e1d60
Kameron Weaver <kaweaver@mines.edu>
2012-04-16 20:04:06 -0600

    added the role id to the users table and columns to the roles table

--- project06/gamez/db/schema.rb
+++ project06/gamez/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120417013845) do
+ActiveRecord::Schema.define(:version => 20120417015528) do
 
   create_table "games", :force => true do |t|
     t.string   "Title"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(:version => 20120417013845) do
     t.string   "photoFile"
     t.integer  "photoSize"
     t.string   "photoType"
+    t.integer  "roleId"
   end
 
 end
